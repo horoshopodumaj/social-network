18f4671841a5ce37b8aed509bbc8770d78021ccf
Diana Murtazina <murka8831@yandex.ru>
2022-08-26 17:26:54 +0400

    getting to know the components

---
+++ some-app/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="App">
+            <Header />
+            <Technologies />
+        </div>
+    );
+};
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    );
+};
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+};
+
+export default App;
