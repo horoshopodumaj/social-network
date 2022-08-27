23a5e01b975cbfc4abd7d4fcb759f4611e562d9b
Diana Murtazina <murka8831@yandex.ru>
2022-08-27 15:45:33 +0400

    the profile component has been moved to a separate file

--- some-app/src/App.js
+++ some-app/src/App.js
@@ -1,24 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
     return (
         <div className="app-wrapper">
             <Header />
             <Navbar />
-            <div className="content">
-                <div>
-                    <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
-                </div>
-                <div> ava + description</div>
-                <div>
-                    My posts
-                    <div>New post</div>
-                    <div>post 1</div>
-                    <div>post 2</div>
-                </div>
-            </div>
+            <Profile />
         </div>
     );
 };
