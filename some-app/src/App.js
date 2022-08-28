f6a8f3ff099defa0e8af3b7e49f639f78d066ba1
Diana Murtazina <murka8831@yandex.ru>
2022-08-28 16:13:44 +0400

    project structure, grouping of components into different folders, header component

--- some-app/src/App.js
+++ some-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/header/Header";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 
