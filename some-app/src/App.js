f486b98a0128cf39001a1f237b9a22810ad89492
Diana Murtazina <murka8831@yandex.ru>
2022-08-30 01:09:34 +0400

    added paths for profile, dialogs elements

--- some-app/src/App.js
+++ some-app/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dialogs from "./components/dialogs/Dialogs";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
@@ -6,15 +7,18 @@ import Profile from "./components/profile/Profile";
 
 const App = () => {
     return (
-        <div className="app-wrapper">
-            <Header />
-            <Navbar />
-
-            <div className="app-wrapper-content">
-                <Dialogs />
-                {/* <Profile /> */}
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header />
+                <Navbar />
+                <div className="app-wrapper-content">
+                    <Routes>
+                        <Route path="/dialogs" element={<Dialogs />} />
+                        <Route path="/profile" element={<Profile />} />
+                    </Routes>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 };
 
