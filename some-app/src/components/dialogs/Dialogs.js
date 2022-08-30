19dfa33ad267b7e3ffc1af604fd14829b3099479
Diana Murtazina <murka8831@yandex.ru>
2022-08-30 19:43:05 +0400

    splitting Dialogs components into separate components

--- some-app/src/components/dialogs/Dialogs.js
+++ some-app/src/components/dialogs/Dialogs.js
@@ -1,36 +1,33 @@
 import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css";
 
+const DialogItem = ({ name, id }) => {
+    return (
+        <div className={`${s.dialog} ${s.active}`}>
+            <NavLink to={`/dialogs/${id}`}>{name}</NavLink>
+        </div>
+    );
+};
+
+const Message = ({ message }) => {
+    return <div className={s.message}>{message}</div>;
+};
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={`${s.dialog} ${s.active}`}>
-                    <NavLink to="/dialogs/1">Jonh</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/2">Max</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/3">Valery</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/4">Diana</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/5">Maria</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/6">Ivan</NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to="/dialogs/7">Mark</NavLink>
-                </div>
+                <DialogItem name="Jonh" id="1" />
+                <DialogItem name="Max" id="2" />
+                <DialogItem name="Valery" id="3" />
+                <DialogItem name="Diana" id="4" />
+                <DialogItem name="Maria" id="5" />
+                <DialogItem name="Ivan" id="6" />
+                <DialogItem name="Mark" id="7" />
             </div>
             <div className={s.messages}>
-                <div className={s.message}>Hi</div>
-                <div className={s.message}>How are you doing?</div>
-                <div className={s.message}>I am ok</div>
+                <Message message="Hi" />
+                <Message message="How are you doing?" />
+                <Message message="I am ok" />
             </div>
         </div>
     );
