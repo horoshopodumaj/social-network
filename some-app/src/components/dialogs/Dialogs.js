38a6ad29af3f791d6047418797d2f2f35d97b2b8
Diana Murtazina <murka8831@yandex.ru>
2022-08-30 16:00:16 +0400

    layout of the Dialogs component

--- some-app/src/components/dialogs/Dialogs.js
+++ some-app/src/components/dialogs/Dialogs.js
@@ -1,7 +1,24 @@
 import s from "./Dialogs.module.css";
 
 const Dialogs = () => {
-    return <div>Dialogs</div>;
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={`${s.dialog} ${s.active}`}>Jonh</div>
+                <div className={s.dialog}>Max</div>
+                <div className={s.dialog}>Valery</div>
+                <div className={s.dialog}>Diana</div>
+                <div className={s.dialog}>Maria</div>
+                <div className={s.dialog}>Ivan</div>
+                <div className={s.dialog}>Mark</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you doing?</div>
+                <div className={s.message}>I am ok</div>
+            </div>
+        </div>
+    );
 };
 
 export default Dialogs;
