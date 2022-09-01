21a31ab0f14093a6dd754ac169b6a789f81a77ef
Diana Murtazina <murka8831@yandex.ru>
2022-09-01 13:17:30 +0400

    avatars added to dialogs

--- some-app/src/components/dialogs/dialogItem/DialogItem.js
+++ some-app/src/components/dialogs/dialogItem/DialogItem.js
@@ -4,6 +4,10 @@ import s from "./../Dialogs.module.css";
 const DialogItem = ({ name, id }) => {
     return (
         <div className={`${s.dialog} ${s.active}`}>
+            <img
+                className={s.img_message}
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgm_ydBb1m56SGa1vwNK4dZshNKWpPSlbCA8GB9QhBxBV3z9213UpzzjHsI25EL1a0r4w&usqp=CAU"
+            />
             <NavLink to={`/dialogs/${id}`}>{name}</NavLink>
         </div>
     );
