695f65d20060f81543fd44f821c2e7d0bc3e5e83
Diana Murtazina <murka8831@yandex.ru>
2022-09-01 15:01:28 +0400

    using ref in the MyPosts element

--- some-app/src/components/profile/myposts/MyPosts.js
+++ some-app/src/components/profile/myposts/MyPosts.js
@@ -1,16 +1,24 @@
 import Post from "./post/Post";
+import React from "react";
 import s from "./MyPosts.module.css";
 
 const MyPosts = ({ state }) => {
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        console.log(text);
+    };
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea ref={newPostElement}></textarea>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
