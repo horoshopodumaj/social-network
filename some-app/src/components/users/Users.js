38ef63ca833883d895cada6dad84ae8613ba1f6f
Diana Murtazina <murka8831@yandex.ru>
2022-09-09 13:18:05 +0400

    transferring the users array to the component (temporary measure)

--- some-app/src/components/users/Users.js
+++ some-app/src/components/users/Users.js
@@ -1,6 +1,37 @@
 import s from "./Users.module.css";
 
 let Users = (props) => {
+    if (props.users.length === 0) {
+        props.setUsers([
+            {
+                id: 1,
+                followed: false,
+                photoUrl:
+                    "https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg",
+                fullName: "Maria",
+                status: "I am a boss",
+                location: { city: "Moscow", country: "Russia" },
+            },
+            {
+                id: 2,
+                followed: true,
+                photoUrl:
+                    "https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg",
+                fullName: "Diana",
+                status: "I am fine",
+                location: { city: "London", country: "Great Britain" },
+            },
+            {
+                id: 3,
+                followed: false,
+                photoUrl:
+                    "https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg",
+                fullName: "Anna",
+                status: "I like football",
+                location: { city: "Oslo", country: "Norway" },
+            },
+        ]);
+    }
     return (
         <div>
             {props.users.map((user) => (
--- some-app/src/redux/users_reducer.js
+++ some-app/src/redux/users_reducer.js
@@ -3,35 +3,7 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 
 let initialState = {
-    users: [
-        {
-            id: 1,
-            followed: false,
-            photoUrl:
-                "https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg",
-            fullName: "Maria",
-            status: "I am a boss",
-            location: { city: "Moscow", country: "Russia" },
-        },
-        {
-            id: 2,
-            followed: true,
-            photoUrl:
-                "https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg",
-            fullName: "Diana",
-            status: "I am fine",
-            location: { city: "London", country: "Great Britain" },
-        },
-        {
-            id: 3,
-            followed: false,
-            photoUrl:
-                "https://klike.net/uploads/posts/2019-03/medium/1551511784_4.jpg",
-            fullName: "Anna",
-            status: "I like football",
-            location: { city: "Oslo", country: "Norway" },
-        },
-    ],
+    users: [],
 };
 
 const usersReducer = (state = initialState, action) => {
