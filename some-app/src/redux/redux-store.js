fe9dda123b90f91a168e251236d02005e52f12eb
Diana Murtazina <murka8831@yandex.ru>
2022-09-15 13:55:05 +0400

    creating a reducer for authorization

--- some-app/src/redux/redux-store.js
+++ some-app/src/redux/redux-store.js
@@ -3,11 +3,13 @@ import { createStore, combineReducers } from "redux";
 import profileReducer from "./profile_reducer";
 import dialogsReducer from "./dialogs_reducer";
 import usersReducer from "./users_reducer";
+import authReducer from "./auth-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
+    auth: authReducer,
 });
 
 let store = createStore(reducers);
