58bdc9a53e42f5affb6a625f07b42f9d1f028579
Diana Murtazina <murka8831@yandex.ru>
2022-09-17 23:34:56 +0400

    added instance

--- some-app/src/api/api.js
+++ some-app/src/api/api.js
@@ -1,45 +1,29 @@
 import * as axios from "axios";
 
+const instance = axios.create({
+    withCredentials: true,
+    headers: {
+        "API-KEY": "b30385df-cbab-4ee6-b2bb-6a3acf152e39",
+    },
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+});
+
 export const getUsers = (currentPage, pageSize) => {
-    return axios
-        .get(
-            `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-            { withCredentials: true }
-        )
+    return instance
+        .get(`users?page=${currentPage}&count=${pageSize}`)
         .then((response) => response.data);
 };
 
 export const followUser = (userId) => {
-    return axios
-        .post(
-            `https://social-network.samuraijs.com/api/1.0/follow/${userId}`,
-            {},
-            {
-                withCredentials: true,
-                headers: {
-                    "API-KEY": "b30385df-cbab-4ee6-b2bb-6a3acf152e39",
-                },
-            }
-        )
-        .then((response) => response.data);
+    return instance.post(`follow/${userId}`).then((response) => response.data);
 };
 
 export const unFollowUser = (userId) => {
-    return axios
-        .delete(
-            `https://social-network.samuraijs.com/api/1.0/follow/${userId}`,
-            {
-                withCredentials: true,
-                headers: {
-                    "API-KEY": "b30385df-cbab-4ee6-b2bb-6a3acf152e39",
-                },
-            }
-        )
+    return instance
+        .delete(`follow/${userId}`)
         .then((response) => response.data);
 };
 
 export const setUser = (userId) => {
-    return axios
-        .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
-        .then((response) => response.data);
+    return instance.get(`profile/${userId}`).then((response) => response.data);
 };
